f6f00b008e25c4f668157e844dc32c4079878c38
Saiganeb <saiganeb@in-Saiganeb.local>
2018-11-30 11:41:37 +0530

    added test cases for create array function

--- test/libraryTest.js
+++ test/libraryTest.js
@@ -8,4 +8,8 @@ describe('createArray', function(){
   it('should return empty array', function(){
     deepEqual(createArray(0,0),[]);
   });
+  it("should return an single element array", function(){
+    deepEqual(createArray(1,0),[0]);
+    deepEqual(createArray(1,'hi'),['hi']);
+  });
 });
